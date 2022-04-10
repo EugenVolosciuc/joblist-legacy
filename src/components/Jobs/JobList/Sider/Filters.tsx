ba992f047a28237e7e88797f25c08c295df33b60
EugenVolosciuc <volosciuc.eugen@gmail.com>
2022-04-10 12:24:50 +0300

    Filtering changes for the job seeker

--- src/components/Jobs/JobList/Sider/Filters.tsx
+++ src/components/Jobs/JobList/Sider/Filters.tsx
@@ -4,6 +4,7 @@ import { Button } from "@chakra-ui/react";
 import Card from "components/shared/Card";
 import { useAuth } from "services/User";
 import { JobPostFilters } from "types/jobPost";
+import { UserRole } from "@prisma/client";
 
 type Props = {
   filters: JobPostFilters;
@@ -21,25 +22,54 @@ const Filters: FC<Props> = ({ filters, setFilters }) => {
     setFilters({ createdById: user?.id });
   };
 
+  const clearFilters = () => {
+    setFilters({});
+  };
+
+  const filtersAreSet = Object.keys(filters).length > 0;
+  const showRecruiterFilters = user?.role === UserRole.RECRUITER;
+  const showJobSeekerFilters = user?.role === UserRole.JOB_SEEKER;
+
   return (
     <Card>
-      {user?.company && (
-        <Button
-          variant="link"
-          onClick={filterByCompanyPosts}
-          isActive={filters?.companyId === user?.companyId}
-        >
-          All {user?.company?.name} job openings
+      {showRecruiterFilters && (
+        <>
+          {user?.company && (
+            <Button
+              variant="link"
+              onClick={filterByCompanyPosts}
+              isActive={filters?.companyId === user?.companyId}
+            >
+              All {user?.company?.name} job openings
+            </Button>
+          )}
+          <Button
+            variant="link"
+            mb="1"
+            onClick={filterByUserPosts}
+            isActive={filters?.createdById === user?.id}
+          >
+            Job posts created by me
+          </Button>
+        </>
+      )}
+      {showJobSeekerFilters && (
+        <>
+          <Button
+            variant="link"
+            mb="1"
+            onClick={clearFilters}
+            isActive={!filtersAreSet}
+          >
+            All available jobs
+          </Button>
+        </>
+      )}
+      {filtersAreSet && (
+        <Button variant="link" mb="1" onClick={clearFilters}>
+          Job posts created by me
         </Button>
       )}
-      <Button
-        variant="link"
-        mb="1"
-        onClick={filterByUserPosts}
-        isActive={filters?.createdById === user?.id}
-      >
-        Job posts created by me
-      </Button>
     </Card>
   );
 };
